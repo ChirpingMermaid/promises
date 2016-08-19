3e659b5168535eb1987d61863ac9928d12b96d0f
chirpingmermaid <bluejello@gmail.com>
2016-08-18 20:52:32 -0700

    callback review and promise constructor completed

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -1,18 +1,42 @@
 /**
  * Implement these functions following the node style callback pattern
  */
-
+var http = require('http');
 var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {
   // TODO
+  var body = [];
+  fs.readFile(filePath, function (err, content) {
+    if (err) {
+      cb(err);
+    }
+    body.push(content);
+    body = Buffer.concat(body).toString().split('\n');
+    cb(null, body[0]);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
   // TODO
+  request(url, function (err, res, body) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, res.statusCode);
+    }
+  });
+  // http.request({ hostname: url, method: 'GET'}, 
+  //   function(res, callback) {
+  //     res.on('error', function(err) {
+  //       callback(err);
+  //     }).on('end', function() {
+  //       callback(null, res.statusCode);
+  //     });
+  //   });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,11 +11,51 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise ( function (resolve, reject) {
+
+    var body = [];
+    fs.readFile(filePath, function (err, content) {
+      if (err) {
+        reject(err);
+      } else {
+        body.push(content);
+        body = Buffer.concat(body).toString().split('\n');
+        resolve(body[0]);
+      }
+    });
+  
+  //     var body = [];
+  // fs.readFile(filePath, function (err, content) {
+  //   if (err) {
+  //     cb(err);
+  //   }
+  //   body.push(content);
+  //   body = Buffer.concat(body).toString().split('\n');
+  //   cb(null, body[0]);
+  // });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return new Promise (function (resolve, reject) {
+    request(url, function(err, res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.statusCode);
+      }
+    });
+  });
+
+  //   request(url, function (err, res, body) {
+  //   if (err) {
+  //     callback(err);
+  //   } else {
+  //     callback(null, res.statusCode);
+  //   }
+  // });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
